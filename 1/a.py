f5b780d8d2ac5ddc3616a643ec8ea2d5f5910fb9
Andrew Barnett <Abarn279@gmail.com>
2020-11-30 23:13:19 -0600

    day 1!

--- 1/a.py
+++ 1/a.py
@@ -1,3 +1,14 @@
+import itertools
+
+def do_sol(inp):
+    combos = itertools.combinations(inp, 3)
+
+    for c in combos:
+        if sum(c) == 2020:
+            return c[0] * c[1] * c[2]
+
 with open('./1.txt') as f:
-    inp = [line.rstrip() for line in f.readlines()]
+    inp = list(map(int, [line.rstrip() for line in f.readlines()]))
+
+print(do_sol(inp))
 
