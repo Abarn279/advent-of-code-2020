031b05fcb5b06828e23d6ed96aa37e37aff4cfad
Andrew Barnett <Abarn279@gmail.com>
2020-12-14 09:09:22 -0600

    less code

--- 13b.py
+++ 13b.py
@@ -1,9 +1,4 @@
-import math
 from collections import deque
-
-def lcm(a, b): 
-    ''' best way to LCM found on stackoverflow. numpy.lcm didnt work for me since it's implemented in C using ints. '''
-    return abs(a*b) // math.gcd(a, b)
 
 with open('./inp/13.txt') as f:
     start = int(f.readline())
@@ -17,7 +12,7 @@ while True:
     time_to_check = time + nxt[0]
 
     if time_to_check % nxt[1] == 0:
-        delta = lcm(delta, nxt[1])
+        delta *= nxt[1]
 
         if len(bus_ids) == 0: 
             break
