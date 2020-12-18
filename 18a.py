c8d70f112e08ad02d3b15f9ef149efa9cd41225b
Andrew Barnett <Abarn279@gmail.com>
2020-12-18 12:32:07 -0600

    18 finished

--- 18a.py
+++ 18a.py
@@ -2,15 +2,34 @@ from collections import deque
 from operator import add, mul
 
 ops = {'*': mul, '+': add}
+op_prio = {'*': 0, '+': 1}
 
 def sort_postfix(expr):
     ''' sort an expression into postfix notation '''
     expr_l = expr.split(' ')
 
-    for i in range(2, len(expr_l), 2): 
-        expr_l[i - 1], expr_l[i] = expr_l[i], expr_l[i - 1]
+    # part 1 sort algo
+    # for i in range(2, len(expr_l), 2): 
+    #     expr_l[i - 1], expr_l[i] = expr_l[i], expr_l[i - 1]
+    # final = expr_l
+
+    # part 2 sort algo - http://www.cs.nthu.edu.tw/~wkhon/ds/ds10/tutorial/tutorial2.pdf
+    final = []
+    stack = deque()
+    for i in expr_l:
+        if i.isdigit(): 
+            final.append(i)
+        else:
+            if len(stack) == 0:
+                stack.appendleft(i)
+            else:
+                while len(stack) > 0 and op_prio[stack[0]] >= op_prio[i]:
+                    final.append(stack.popleft())
+                stack.appendleft(i)
+    for i in stack:
+        final.append(i)
     
-    return ' '.join(expr_l)
+    return ' '.join(final)
 
 def do_calc(expr):
 
@@ -29,7 +48,7 @@ def do_calc(expr):
         expr = pre + solved + post
         return do_calc(expr)
 
-    # sort into postfix queue, create stack
+    # sort into postfix notation queue, create stack, evaluate
     q = deque(sort_postfix(expr).split(' '))
     s = deque()
 
