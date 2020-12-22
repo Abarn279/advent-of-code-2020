6b37f4d70558c1cf73a3b9a46b3f2c72e4630c11
Andrew Barnett <Abarn279@gmail.com>
2020-12-22 12:41:24 -0600

    shorter comments

--- 22b.py
+++ 22b.py
@@ -13,29 +13,28 @@ def simulate_combat(p1, p2):
 
     while len(p1) > 0 and len(p2) > 0:
 
-        # Before either player deals a card, if there was a previous round in this game that had exactly the same cards in the same order in the same players' decks, the game instantly ends in a win for player 1. Previous rounds from other games are not considered.
+        # check if repeat round
         round_rep = get_round_rep(p1, p2, game)
         if round_rep in seen_rounds:
             return 1
         seen_rounds.add(round_rep)
 
-        # Otherwise, this round's cards must be in a new configuration; the players begin the round by each drawing the top card of their deck as normal.
+        # draw cards
         c1, c2 = p1.popleft(), p2.popleft()
 
-        # If both players have at least as many cards remaining in their deck as the value of the card they just drew, the winner of the round is determined by playing a new game of Recursive Combat
+        # need recursive?
         if len(p1) >= c1 and len(p2) >= c2:
             p1_c, p2_c = deque(list(p1)[:c1]), deque(list(p2)[:c2])
             winner = simulate_combat(p1_c, p2_c)
             if winner == 1: p1.append(c1);p1.append(c2)
             elif winner == 2: p2.append(c2);p2.append(c1)
 
-        # Otherwise, at least one player must not have enough cards left in their deck to recurse; the winner of the round is the player with the higher-value card.
+        # regular non-recursive round
         else:
             if c1 > c2: p1.append(c1);p1.append(c2)
             elif c1 < c2: p2.append(c2);p2.append(c1)
 
     return 1 if len(p1) > 0 else 2
-
 
 # Get input
 with open('./inp/22.txt') as f:
